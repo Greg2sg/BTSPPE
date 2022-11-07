417f47a56cb02c18a580b7b126c8217615aece2b
MasterGorge <103949325+MasterGorge@users.noreply.github.com>
2022-11-07 15:53:56 +0100

    martin

--- form/conn.php
+++ form/conn.php
@@ -4,12 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/styleConn.css"/> 
     <title>Formulaire de connexion</title>
 </head>
 <body>
-    <form>
-        <label name="identifiants" >Identifiants</label>$
-        <input type="text" name="identifiant">
+    <form method="post" action="conn.php">
+        <div>
+            <legend class="ok">Identifiants</legend>
+            <input type="text" name="identifiant" required/>
+        </div>
+        <div>
+            <legend>Mots de passe</legend>
+            <input type="password" name="password" required/>
+        </div>
+        <div>
+            <input type="submit" name="envoyer" required/>
+        </div>
+        
     </form>
 </body>
 </html>

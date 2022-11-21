0787ebd775f6db23d5bc29dddda089002d250cc1
Greg <gregory.desorgues@hotmail.fr>
2022-11-21 16:49:53 +0100

    dbmamp

--- form/db_mamp.php
+++ form/db_mamp.php
@@ -1,10 +1,10 @@
 <?php
 
 $user= 'root';
-$password='';
+$password='root';
 
 try{
-    $conn = new PDO('mysql:host=localhost;dbname=gsb_bdd;charset=utf8', $user, $password );
+    $conn = new PDO('mysql:host=localhost:8889;dbname=gsb_bdd;charset=utf8', $user, $password );
     //echo'Connexion réussi';
 } catch(PDOException $e){
     die('Erreur: impossible de se connecter à la base de donnée');

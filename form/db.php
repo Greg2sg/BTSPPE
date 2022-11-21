1fe25dcc3dd68032aceff777514d3998e7ffb177
MrLeCoquet <t.carreira.other@icloud.com>
2022-11-21 18:03:38 +0100

    tim

--- form/db.php
+++ form/db.php
@@ -5,7 +5,7 @@ $password='root';
 
 try{
     $conn = new PDO('mysql:host=localhost:8889;dbname=gsb_bdd;charset=utf8', $user, $password );
-    echo'Connexion réussi';
+   // echo'Connexion réussi';
 } catch(PDOException $e){
     die('Erreur: impossible de se connecter');
 }

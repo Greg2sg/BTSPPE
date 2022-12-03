68fe4271b37af2a432ccd158fe20391b364041a2
MasterGorge <103949325+MasterGorge@users.noreply.github.com>
2022-12-03 14:11:48 +0100

    Martin

--- form/editprofil.php
+++ form/editprofil.php
@@ -1,8 +1,5 @@
 <?php
 session_start();
- 
-if(isset($_GET['id']) AND $_GET['id'] > 0) {
-   $userinfo = $_SESSION;
 
 ?>
 
@@ -12,37 +9,37 @@ if(isset($_GET['id']) AND $_GET['id'] > 0) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Editer</title>
 </head>
 <body>
     <form method="POST" action="editprofil.php">
         <div>
             <label>Nom: </label>
-            <input type="text" name="nom" required>
+            <input type="text" name="nom" value="<?php echo $userinfo['Nom']?>" required>
         </div>
         <div>
             <label>prenom: </label>
-            <input type="text" name="prenom" required>
+            <input type="text" name="prenom" value="<?php echo $_SESION['Prenom']?>" required>
         </div>
         <div>
             <label>Rôle: </label>
-            <input type="text" name="role" required>
+            <input type="text" name="role" value="<?php echo $_SESION['Role']?>" required>
         </div>
         <div>
             <label>Email: </label>
-            <input type="email" name="email" required>
+            <input type="email" name="email" value="<?php $userinfo['email'];?>" required>
         </div>
         <div>
             <label>Poste: </label>
-            <input type="text" name="poste" required>
+            <input type="text" name="poste" value="<?php echo $_SESION['Poste']?>" required>
         </div>
         <div>
             <label>Responsable: </label>
-            <input type="text" name="responsable" required>
+            <input type="text" name="responsable" value="<?php echo $_SESION['Responsable']?>" required>
         </div>
         <div>
             <label>Password: </label>
-            <input type="password" name="password" required>
+            <input type="password" name="password" value="" required>
         </div>
         <input type="submit" name="editer">
     </form>
@@ -56,15 +53,11 @@ if(isset($_POST['editer'])){
     $poste = $_POST['poste'];
     $role = $_POST['role'];
     $responsable = $_POST['responsable'];
-
-    $data = "SELECT * FROM user" ;
-    $sql = "UPDATE user SET Role =$role ,Prenom = $prenom ,Nom =$nom ,Mail = $email ,Mdp = $password ,Responsable = $responsable ,Poste = $poste WHERE 1";
+    $sql = "UPDATE user SET Role = $role ,Prenom = $prenom ,Nom =$nom ,Mail = $email ,Mdp = $password ,Responsable = $responsable ,Poste = $poste";
     $req= $conn->prepare($sql);
-    $req->execute($data);
+    $req->execute();
+    header("location:profil.php");
 }
 ?>
 </body>
 </html>
-<?php
-}
-?>

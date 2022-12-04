b59241f88459807fb4a892399f009de25cf16b6a
Rayan69100 <yayan1@live.fr>
2022-12-04 15:20:27 +0100

    ok

--- form/fichedefrais2.php
+++ form/fichedefrais2.php
@@ -1,56 +1,126 @@
 <!DOCTYPE html>
-<!-- Created By CodingNepal -->
-<html lang="en" dir="ltr">
+
+<html lang="fr" >
    <head>
       <meta charset="utf-8">
-      <title>Responsive Contact us Form | CodingNepal</title>
-      <link rel="stylesheet" href="style.css">
+      <title>Saisie frais forfaitaires</title>
+      <link rel="stylesheet" href="../css/frais.css">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
    </head>
    <body>
     <div class="container">
-        <div class="text">Responsive Contact us Form</div>
-        <form action="#">
-           <div class="form-row">
-              <div class="input-data">
-                 <input type="text" required>
+        <div class="text">Bienvenue chez GSB</div>
+        <form method="POST" action="fichedefrais2.php" name="Visiteur">
+
+        <div class="form-row">
+        <div class="input-data">
+              <input type="number" name="mois" min="1" max="12" required>
                  <div class="underline"></div>
-                 <label for="">First Name</label>
+                 <label for=""><h3>Saisie fiche de frais pour le mois :</h3></label>
+              </div></div>
+
+
+        <div class="form-row">
+              <div class="input-data">
+              <input type="text" name="nom" required>
+                 <div class="underline"></div>
+                 <label for="">Nom:</label>
               </div>
               <div class="input-data">
-                 <input type="text" required>
+              <input type="text" name="prenom" required>
                  <div class="underline"></div>
-                 <label for="">Last Name</label>
+                 <label for="">Prénom:</label>
               </div>
            </div>
            <div class="form-row">
               <div class="input-data">
-                 <input type="text" required>
+              <input type="text" name="poste" required>
                  <div class="underline"></div>
-                 <label for="">Email Address</label>
+                 <label for="">Poste:</label>
               </div>
-              <div class="input-data">
-                 <input type="text" required>
+              <div class="input-data2">
+              <input type="date" name="date" required>
                  <div class="underline"></div>
-                 <label for="">Website Name</label>
+                 <label for="">Date:</label>
               </div>
            </div>
            <div class="form-row">
               <div class="input-data textarea">
-                 <textarea rows="8" cols="80" required></textarea>
+                 
                  <br />
                  <div class="underline"></div>
-                 <label for="">Write your message</label>
+                 <label for="">Frais Forfaitaires :</label>
+                 
                  <br />
+                 <div class="form-row">
+              <div class="input-data">
+               <input type="number" name="hebergement" placeholder="En euros" min="0" required>
+                 <div class="underline"></div>
+                 <label for="">Hébergement:</label>
+              </div>
+              <div class="input-data">
+              <input type="number" name="repas" placeholder="En euros" required>
+                 <div class="underline"></div>
+                 <label for="">Repas:</label>
+              </div>
+              
+           </div>
+           <div class="form-row">
+              <div class="input-data">
+               <input type="number" name="transport" placeholder="En euros" min="0" required>
+                 <div class="underline"></div>
+                 <label for="">Transport:</label>
+              </div>
+              <div class="input-data">
+              <input type="number" name="prix_total" placeholder="En euros" min="0" required>
+                 <div class="underline"></div>
+                 <label for="">Prix Total:</label>
+              </div>
+                 
+                 </div>
                  <div class="form-row submit-btn">
                     <div class="input-data">
                        <div class="inner"></div>
-                       <input type="submit" value="submit">
-                    </div>
-                 </div>
+                       <input type="submit" name="Envoyer">
+                    </div></div>
               </div>
            </div>
         </form>
      </div>
    </body>
+</html>
+
+<?php
+    //Connexion à la base de donnée
+    include "db.php";
+
+
+if(isset($_POST['Envoyer'])) 
+{
+
+  //Déclaration des variables
+    $date = $_POST['date'];
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $poste =$_POST['poste'];
+    $mois = $_POST['mois'];
+    $hebergement = $_POST['hebergement'];
+    $repas = $_POST['repas'];
+    $transport = $_POST['transport'];
+    $prixtotal = $_POST['prix_total'];
+
+    //Insérer les données dans la base de données
+    $req = $conn->prepare("INSERT INTO `fichefrais`( `nom`, `prenom`, `poste`, `mois`, `date`, `hebergement`, `repas`, `transport`, `prix_total`) VALUES ('$nom','$prenom','$poste','$mois','$date','$hebergement','$repas','$transport','$prixtotal')");
+    $res = $req ->execute();
+
+    if($res){
+      echo "envoie réussi";
+    }else{
+      echo "rien ne marche";
+    }
+
+}
+
+?>
+</body>
 </html>
--- index2.php
+++ index2.php
@@ -20,7 +20,7 @@ if(isset($_GET['id']) AND $_GET['id'] > 0) {
     <header class="header">
         <h1 class="logo">Logo</h1>
         <ul class="nav">
-          <li><a href="form/fichedefrais.php">Fiche de frais</a></li>
+          <li><a href="form/fichedefrais2.php">Fiche de frais</a></li>
           <!-- <li><a href="form/inscription.php">Inscription</a></li> -->
           <li><a href="form/profil.php?id=<?php echo $_SESSION['id'] ?>">Profil</a></li>
           <li><a href="form/propos.php">A propos</a></li>

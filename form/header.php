9a2b9619ecbff423d0be412142bb2cca7efaa6b3
MasterGorge <103949325+MasterGorge@users.noreply.github.com>
2022-12-12 18:12:55 +0100

    Martin

--- form/header.php
+++ form/header.php
@@ -17,19 +17,24 @@ session_start();
     <header class="header">
         <h1 class="logo">Logo</h1>
         <ul class="nav">
+            
            <!-- Afficher lorsque l'on est connecter -->
-            <?php if(isset($_SESSION['id'])): ?>
-            <li><a href="fichedefrais.php?id=<?php echo $_SESSION['id'] ?>">Fiche de frais</a></li> 
-            <li><a href="note_de_frais.php?id=<?php echo $_SESSION['id'] ?>">Note de frais</a></li>
-            <li><a href="profil.php?id=<?php echo $_SESSION['id'] ?>">Profil</a></li>
+            <?php if(isset($_SESSION['id'])){
+                if($userinfo['id_role'] == 3){?>
+                    <li><a href="form/inscription.php">Inscription</a></li>
+                <?php } ?>
+                
+            <li><a href="form/fichedefrais.php?id=<?php echo $_SESSION['id'] ?>">Fiche de frais</a></li> 
+            <li><a href="form/note_de_frais.php?id=<?php echo $_SESSION['id'] ?>">Note de frais</a></li>
+            <li><a href="form/profil.php?id=<?php echo $_SESSION['id'] ?>">Profil</a></li>
             
             <!-- Afficher si l'on est pas connecter -->
-            <?php else: ?>
-            <li><a href="inscription.php">Inscription</a></li> 
-            <li><a href="conn.php">Connexion</a></li>
+            <?php }else{  ?>
+             
+            <li><a href="form/conn.php">Connexion</a></li>
 
-            <?php endif; ?>
-            <li><a href="propos.php">A propos</a></li>
+            <?php }; ?>
+            <li><a href="form/propos.php">A propos</a></li>
         </ul>
     </header>
 

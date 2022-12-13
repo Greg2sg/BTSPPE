4d741adf5603ace520bef7146779b7d0e35e3a45
MasterGorge <103949325+MasterGorge@users.noreply.github.com>
2022-12-13 15:53:04 +0100

    Martin

--- form/note_de_frais.php
+++ form/note_de_frais.php
@@ -43,6 +43,7 @@ echo "
             <th>Repas </th> 
             <th>Hebergement</th> 
             <th>Autres</th> 
+            <th>Modification</th> 
 
             
             </tr>
@@ -59,6 +60,7 @@ while($donnee = $req->fetch()){
                             <td>".$donnee['repas']." euro</td>
                             <td>".$donnee['hebergement']." euro</td>
                             <td>".$donnee['autres']." euro</td>
+                            <td><a href='editfichefrais.php'>Editer</a></td>
                             
                         </tr>
                         

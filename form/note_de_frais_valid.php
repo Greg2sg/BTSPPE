b71bbbb8a93ad08591d0639ae9b20d0312fef0bb
MasterGorge <103949325+MasterGorge@users.noreply.github.com>
2022-12-13 16:54:32 +0100

    Martin

---
+++ form/note_de_frais_valid.php
@@ -0,0 +1,76 @@
+<?php
+//session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<style>
+    table,td,th, tr{
+        border: 1px solid;
+        color: black;
+    }
+    h1{
+        color: black;
+    }
+</style>
+<?php 
+//include "header.php";
+$req = $conn->prepare("SELECT * FROM user ");
+$req->execute();
+$user = $req->fetch();
+?>
+
+<body>
+    <h1>Note de Frais de <?php echo $user['nom']; ?> <?php echo $user['prenom']; ?></h1>
+<?php
+//Connexion à la base de donnée
+include "db.php";
+
+//Récupérer les donnée de la table fiche frais
+$req = $conn->prepare("SELECT * FROM fichefrais WHERE ID_USER = :ID_user");
+$req->execute(array(':ID_user'=>$_SESSION['id']));
+
+//Mise en forme du tableau de note de frais
+echo "  
+    <table>
+        <thead>
+            <tr>
+            <th>date</th>  
+            <th>description</th> 
+            <th>Deplacement</th> 
+            <th>Repas </th> 
+            <th>Hebergement</th> 
+            <th>Autres</th> 
+            <th>Etat</th>
+            <th>Modifier</th>
+
+            </tr>
+            
+    </thead>";
+
+
+while($donnee = $req->fetch()){
+            echo "  <tbody>
+                        <tr>
+                            <td>".$donnee['date']."</td>
+                            <td>".$donnee['description']." </td>
+                            <td>".$donnee['transport']." euro</td>
+                            <td>".$donnee['repas']." euro</td>
+                            <td>".$donnee['hebergement']." euro</td> 
+                            <td>".$donnee['autres']." euro</td>
+                            <td>".$donnee['etat']."</td>
+                            <td><a href='editfichefrais.php'>editer</a></td>
+                        </tr>
+                        
+                    </tbody>";
+}
+?>
+<button onClick="javascript:document.location.href='../index.php'">Retour</button>
+</body>
+</html>
---
+++ form/validation.php
@@ -0,0 +1,61 @@
+<?php
+//session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<style>
+    table,td,th, tr{
+        border: 1px solid;
+        color: black;
+    }
+    h1{
+        color: black;
+    }
+</style>
+<?php 
+include "header.php";
+?>
+<body>
+    <h1>Note de Frais de <?php echo $_SESSION['nom']; ?> <?php echo $_SESSION['prenom']; ?></h1>
+<?php
+//Connexion à la base de donnée
+include "db.php";
+
+//Récupérer les donnée de la table 
+$req = $conn->prepare("SELECT * FROM user ");
+$req->execute();
+
+//Mise en forme du tableau de note de frais
+echo "  
+    <table>
+        <thead>
+            <tr>
+            <th>Nom</th>  
+            <th>Prenom</th> 
+            <th>Validation</th>
+            </tr>
+            
+    </thead>";
+
+
+while($donnee = $req->fetch()){
+            echo "  <tbody>
+                        <tr>
+                            <td>".$donnee['Nom']."</td>
+                            <td>".$donnee['Prenom']." </td>
+                            <td><a href='note_de_frais_valid.php?id=".$donnee['ID_User']."'>validation</a></td>
+                        </tr>
+                        
+                    </tbody>";
+}
+?>
+<button onClick="javascript:document.location.href='../index.php'">Retour</button>
+</body>
+</html>

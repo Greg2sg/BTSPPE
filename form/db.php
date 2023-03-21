a9ff4497e8d6e5046394261244a434693b583fbf
Rayan69100 <yayan1@live.fr>
2023-03-21 17:27:57 +0100

    ok

--- form/db.php
+++ form/db.php
@@ -1,11 +1,11 @@
 <?php
 
-$user= 'timothzbtsppe';
-$password='jesuisTim2414';
+$user= 'root';
+$password='';
 
 //Connexion à la base de donnée
 try{
-    $conn = new PDO('mysql:host=timothzbtsppe.mysql.db;dbname=timothzbtsppe;charset=utf8', $user, $password );
+    $conn = new PDO('mysql:localhost:host;dbname=gsb_bdd;charset=utf8', $user, $password );
     // echo'Connexion réussi';
 } catch(PDOException $e){
     die('Erreur: impossible de se connecter');

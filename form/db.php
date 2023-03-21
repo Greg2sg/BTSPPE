71905fc7dbaa6a2f8d4474d4d5625f5cee0ccf78
Rayan69100 <yayan1@live.fr>
2023-03-21 17:40:40 +0100

    ok

--- form/db.php
+++ form/db.php
@@ -5,7 +5,7 @@ $password='';
 
 //Connexion à la base de donnée
 try{
-    $conn = new PDO('mysql:localhost:host;dbname=gsb_bdd;charset=utf8', $user, $password );
+    $conn = new PDO('mysql:localhost=host;dbname=gsb_bdd;charset=utf8', $user, $password );
     // echo'Connexion réussi';
 } catch(PDOException $e){
     die('Erreur: impossible de se connecter');
